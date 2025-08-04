5443f5361df7b6815d38ebe6c9bf1b9f152d7e6e
ahmadNoval <149749575+Vall-Here@users.noreply.github.com>
2025-08-04 10:06:02 +0700

    up : add new tabel

---
+++ app/Database/Migrations/2025-08-04-025432_CreateJenisPerlengkapan.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateJenisPerlengkapan extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nama_perlengkapan' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'jenis_perlengkapan' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+             'created_by' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+                'null'       => true,
+            ],
+            'updated_by' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+                'null'       => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('tb_jenis_perlengkapan');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tb_jenis_perlengkapan', true);
+    }
+}
